a7e46dbc1833f078b50f262830a78424aca501d3
Gustavo <codergustavo@gmail.com>
2024-07-02 20:14:33 -0300

    commit

--- src/config/database.js
+++ src/config/database.js
@@ -1,18 +1,18 @@
 
-// module.exports = {
-//   dialect: "mysql",
-//   host: "db-mysql-nyc3-21038-do-user-16809639-0.c.db.ondigitalocean.com",
-//   username: "doadmin",
-//   password: "AVNS_wieu-YYjsclCV4fzrVc",
-//   database: "doctors4u",
-//   port: 25060,
+module.exports = {
+  dialect: "mysql",
+  host: "db-mysql-nyc3-21038-do-user-16809639-0.c.db.ondigitalocean.com",
+  username: "doadmin",
+  password: "AVNS_wieu-YYjsclCV4fzrVc",
+  database: "doctors4u",
+  port: 25060,
 
-//   pool: {
-//     max: 5,
-//     min: 0,
-//     idle: 10000,
-//   },
-// };
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000,
+  },
+};
 
 
 // module.exports = {
